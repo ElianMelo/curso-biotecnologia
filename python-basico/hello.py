91799edbd2a821ec6d5f693d3893bc77c73fcf8f
ElianMelo <elianmelomorais@gmail.com>
2019-12-04 11:18:49 -0300

    Estudo de variaveis e comentarios

---
+++ python-basico/hello.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Meu primeiro programa
+# Linhas comentadas nao sao processadas
+
+"""
+Comentário
+de muitas
+linhas
+"""
+
+print("Hello World/ Olá Mundo")
---
+++ python-basico/variaveis.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+var1 = 1 # variável inteira
+var2 = 1.1 # variável float
+var3 = "Eu sou uma string" # variável string
+var4 = True # verdadeiro
+var4 = False # sobrecrita de variável
+var5 = False # falso
+
+print(var1)
+print(var2)
+print(var3)
+print(var4)
+print(var5)
