cb910fe4e66794e753379bba7225ad1ef2ec079e
ElianMelo <elianmelomorais@gmail.com>
2019-12-13 08:50:34 -0300

    Estudo de Strings e suas propriedades

---
+++ python-intermediario/Strings.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+a = "Elian"
+b = "Melo"
+
+concatenar = a + " " + b
+
+print(concatenar)
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+print(a[4])
+print(concatenar[0:5])
