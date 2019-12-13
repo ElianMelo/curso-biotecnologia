117e1a93515adff8db7d9a68a4f11c52c2868ed9
ElianMelo <elianmelomorais@gmail.com>
2019-12-13 09:13:58 -0300

    Estudo de funcoes e suas caracteristicas

---
+++ python-intermediario/Funcoes.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+
+def soma(x, y):
+    return x + y
+
+
+def multiplicacao(x, y):
+    return x * y
+
+
+s = soma(1, 2)
+m = multiplicacao(3, 4)
+print(s)
+print(m)
+print(soma(s, m))
